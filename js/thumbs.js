3e519f0ea5cade060601f4cdadd15dd05db46177
Alexandra0107 <alexandra.awerjanova@yandex.ru>
2026-02-18 08:17:12 +0300

    правки для линтера

--- js/thumbs.js
+++ js/thumbs.js
@@ -3,7 +3,9 @@ import { NAMES, MESSAGES } from './data.js';
 
 const ARRAY_LEN = 25;
 
-const generateId = (() => { let id = 1; return () => id++; })();
+const generateId = (() => {
+   let id = 1; return () => id++;
+  })();
 
 // Функция: генерация ОДНОГО комментария (объекта)
 function generateComment() {
