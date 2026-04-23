f15b739a6f17855d02b17800d780404ad7ebac6a
Alexandra0107 <167964471+Alexandra0107@users.noreply.github.com>
2026-04-23 08:10:40 +0000

    правки пробелов

--- js/main.js
+++ js/main.js
@@ -14,15 +14,14 @@ const picturesContainer = document.querySelector('.pictures');
 renderThumbs(thumbsList, template, picturesContainer);
 // Добавляем обработчики клика на миниатюры
 picturesContainer.addEventListener('click', (evt) => {
-const thumbnail = evt.target.closest('.picture');
- if (!thumbnail) {
+  const thumbnail = evt.target.closest('.picture');
+  if (!thumbnail) {
     return;
- }
+  }
+  const index = Number(thumbnail.dataset.id);
+  const photoData = thumbsList[index];
 
-const index = Number(thumbnail.dataset.id);
-const photoData = thumbsList[index];
-
-if (photoData) {
+  if (photoData) {
     openFullScreen(photoData);
-}
+  }
 });
